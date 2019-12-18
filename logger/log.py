6aebd2cc2a074fa5247223fd9b5f4a407e2537f5
Marco Heiming <myx00r@googlemail.com>
2019-12-18 11:29:27 +0100

    Python file format & improvements

--- logger/log.py
+++ logger/log.py
@@ -3,6 +3,8 @@
 #   @date 2018-06-09 17:30
 #   @author Marco Heiming
 #
+#   https://github.com/myxor/sensorlog
+#
 
 
 import sqlite3
@@ -14,9 +16,15 @@ import json
 import os.path
 import configparser
 import importlib
+# Look for DHT support
+from importlib import util
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+db_path = ""
+api_host = ""
+api_port = ""
 
 try:
     db_path = config['DB']['PATH']
@@ -26,29 +34,29 @@ except KeyError:
     print("Error: Needed path in config.ini not found.")
     exit()
 
-if (db_path == ""):
+if db_path == "":
     print("Error: DB.PATH not set in config.ini")
     exit()
 
-if (api_host == ""):
+if api_host == "":
     print("Error: API.HOST not set in config.ini")
     exit()
-if (api_port == ""):
+if api_port == "":
     print("Error: API.PORT not set in config.ini")
     exit()
 
-if (len(sys.argv) < 2):
+if len(sys.argv) < 2:
     print("Usage: python3 log.py (sqlite|restful)")
     exit()
 
-type = sys.argv[1]  # "sqlite" or "restful"
+logtype = sys.argv[1]  # "sqlite" or "restful"
 
-if type == "sqlite":
+if logtype == "sqlite":
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS temperatures(datetime text, sensor_id text, value real)''')
     c.execute('''CREATE TABLE IF NOT EXISTS humidities(datetime text, sensor_id text, value real)''')
-elif type == "restful":
+elif logtype == "restful":
     url = "http://" + api_host + ":" + api_port + "/"
 else:
     print("Usage: python3 log.py (sqlite|restful)")
@@ -79,22 +87,22 @@ def get1wire():
 
                 # do not log values > 70 °C:
                 if temperature <= 70:
-                    logTemp(str(w1_slave), str(temperature))
+                    log_temperature(str(w1_slave), str(temperature))
     else:
         print("No 1wire support")
 
 
-def logTemp(sensor_id, temperature):
+def log_temperature(sensor_id, temperature):
     print("logTemp(" + sensor_id + ", " + temperature + ")")
     now = datetime.now(timezone.utc)
 
-    if type == "sqlite":
+    if logtype == "sqlite":
         # write into DB:
         c.execute("INSERT INTO temperatures VALUES ('" + now.isoformat() + "','" + str(sensor_id) + "', " + str(
             temperature) + ")")
         print("saved to sqlite")
 
-    if type == "restful":
+    if logtype == "restful":
         # send to RESTful API:
         data = {"datetime": now.isoformat(), "sensor_id": str(sensor_id), "value": str(temperature)}
         data_json = json.dumps(data)
@@ -105,17 +113,17 @@ def logTemp(sensor_id, temperature):
         print("HTTP response", response)
 
 
-def logHumi(sensor_id, humidity):
+def log_humidity(sensor_id, humidity):
     print("logHumi(" + sensor_id + ", " + humidity + ")")
     now = datetime.now(timezone.utc)
 
-    if type == "sqlite":
+    if logtype == "sqlite":
         # write into DB:
         c.execute(
             "INSERT INTO humidities VALUES ('" + now.isoformat() + "','" + str(sensor_id) + "', " + str(humidity) + ")")
         print("saved to sqlite")
 
-    if type == "restful":
+    if logtype == "restful":
         # send to RESTful API:
         data = {"datetime": now.isoformat(), "sensor_id": str(sensor_id), "value": str(humidity)}
         data_json = json.dumps(data)
@@ -126,32 +134,29 @@ def logHumi(sensor_id, humidity):
         print("HTTP response", response)
 
 
-def getDHT():
+def get_dht22_values():
     import Adafruit_DHT
     sensor = Adafruit_DHT.DHT22
     pin = 4
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     print("Humidity: " + str(humidity) + "%, temperature: " + str(temperature) + "°C")
     sensor_id = "DHT" + str(sensor) + str(pin)
-    if (humidity is not None):
-        logHumi(sensor_id, str(round(humidity, 3)))
-    if (temperature is not None):
-        logTemp(sensor_id, str(round(temperature, 3)))
+    if humidity is not None:
+        log_humidity(sensor_id, str(round(humidity, 3)))
+    if temperature is not None:
+        log_temperature(sensor_id, str(round(temperature, 3)))
 
 
 get1wire()
 
-# Look for DHT support
-from importlib import util
-
 dht = importlib.util.find_spec("Adafruit_DHT")
-dht_supported = dht is not None
-if (dht_supported):
-    getDHT()
+if dht is not None:
+    get_dht22_values()
 else:
     print("No DHT support")
 
 # finalize:
-if type == "sqlite":
-    conn.commit()
-    conn.close()
+if logtype == "sqlite":
+    if conn:
+        conn.commit()
+        conn.close()
